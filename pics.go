5ea943bfd7c5a54ea5d712cd06b295100410a33c
Joshua Uduehi <juduehi41@gmail.com>
2020-12-19 06:54:37 +0000

    added file for pictures of each side of dice

---
+++ pics.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func drawOne() {
+  fmt.Println("\n")
+  fmt.Println(" _______")
+  fmt.Println("/______/|")
+  fmt.Println("|      ||")
+  fmt.Println("|  1   ||")
+  fmt.Println("|______|/")
+}
+func drawTwo() {
+  fmt.Println("\n")
+  fmt.Println(" _______")
+  fmt.Println("/______/|")
+  fmt.Println("|      ||")
+  fmt.Println("|  2   ||")
+  fmt.Println("|______|/")
+}
+func drawThree() {
+  fmt.Println("\n")
+  fmt.Println(" _______")
+  fmt.Println("/______/|")
+  fmt.Println("|      ||")
+  fmt.Println("|  3   ||")
+  fmt.Println("|______|/")
+}
+func drawFour() {
+   fmt.Println("\n")
+  fmt.Println(" _______")
+  fmt.Println("/______/|")
+  fmt.Println("|      ||")
+  fmt.Println("|  4   ||")
+  fmt.Println("|______|/")
+}
+func drawFive() {
+  fmt.Println("\n")
+  fmt.Println(" _______")
+  fmt.Println("/______/|")
+  fmt.Println("|      ||")
+  fmt.Println("|  5   ||")
+  fmt.Println("|______|/")
+}
+func drawSix() {
+  fmt.Println("\n")
+  fmt.Println(" _______")
+  fmt.Println("/______/|")
+  fmt.Println("|      ||")
+  fmt.Println("|  6   ||")
+  fmt.Println("|______|/")
+}
